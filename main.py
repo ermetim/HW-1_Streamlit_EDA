806754f30ade53e06cce9a61ea32f2b8c4f6870e
ermetim <ermetim@gmail.com>
2023-12-10 13:11:29 +0300

    First commit. Creat main.py Make first hist

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import plotly
 
 # заголовок приложения
 st.title('Домашнее задание по прикладному Python')
 
 df = pd.read_csv('datasets/joined_dataset.csv')
-# st.text("Введите ваш рост")
+
+# построение графиков распределений признаков
+
+# st.text("Построим графики распределения признаков")
+st.header("1. Построим графики распределения признаков")
 
 columns = list(df.columns)
 columns.remove('AGREEMENT_RK')
@@ -29,15 +34,51 @@ col_dict = {'AGREEMENT_RK' : 'Уникальный идентификатор о
 option = st.selectbox(
      'Выберите параметр, по которому хотите посмотреть распределение',
      (columns))
-st.write(f'You selected: {option} - {col_dict[option]}')
+st.write(f'Вы выбрали: {option} - {col_dict[option]}')
 
+bins = st.number_input("Выберите количество корзин для  распределения",20, 200)
 
-# fig, ax = plt.subplots()
-# ax.hist(df[option], bins=50)
-# plt.grid('all')
-#
-# fig
+title_name = col_dict[option].split("(")[0]
+if len(col_dict[option].split("(")) < 2:
+    subtitle_name = ""
+else:
+    subtitle_name = col_dict[option].split("(")[-1][:-1]
 
-plt.hist(df[option], bins=50)
+fig, ax = plt.subplots()
+plt.title(f'График распределения по параметру\n{title_name}\n{subtitle_name}')
+ax.hist(df[option], bins=bins)
+
+# ax.set_xlabel('Имя', loc='right')
+ax.set_ylabel('Количество')
 plt.grid('all')
-# plt.show()
+
+fig
+
+# построение графиков зависимостей целевой переменной и признаков
+st.header("2. Построим графики зависимостей целевой переменной и признаков")
+dep_cols = columns.copy()
+dep_cols.remove('TARGET')
+
+# Select box
+option1 = st.selectbox(
+     'Выберите параметр, от которого хотите посмотреть зависимость целевой переменной',
+     (dep_cols))
+
+st.write(f'Ваш выбор: {option1} - {col_dict[option1]}')
+title_name = col_dict[option1].split("(")[0]
+if len(col_dict[option1].split("(")) < 2:
+    subtitle_name = ""
+else:
+    subtitle_name = col_dict[option1].split("(")[-1][:-1]
+
+fig, ax = plt.subplots()
+plt.title(f'График зависимости целевой переменной от параметра\n{title_name}\n{subtitle_name}')
+ax.scatter(df[option1], df['TARGET'])
+
+# ax.set_xlabel('Имя', loc='right')
+# ax.set_ylabel('Целевая переменная')
+plt.grid('all')
+
+fig
+
+
